b859d4362470f949e68ee1a7a967feb0b99827b8
eletanzo <eletanzo@gmail.com>
2022-10-31 10:20:59 -0400

    first commit

---
+++ library-check.py
@@ -0,0 +1,16 @@
+import requests
+from xml.dom import minidom
+
+token = '7e_C17zcFPXZ42MEZw1j'
+
+res = requests.get(f"http://brokeserver.live:32400/library/sections/1/all?X-Plex-Token=${token}")
+xml = minidom.parse(res.text)
+movies = xml.getElementsByTagName('Video')
+
+res = requests.get(f"http://brokeserver.live:32400/library/sections/2/all?X-Plex_token=${token}")
+xml = minidom.parse(res.text)
+shows = xml.getElementsByTagName('Video')
+
+print(f"# Movies: ${len(movies)}")
+print(f"# Shows: ${len(shows)}")
+
